99c32ba5ac785f69d7859e7edaa6df6eb3042288
akinisik <akin.isik@windowslive.com>
2019-11-07 02:13:07 +0300

    Banking microservice initialize

---
+++ MicroServiceWithRabbitMQ/Banking.Application/Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using Banking.Application.Interfaces;
+using Banking.Domain.Interfaces;
+using Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepository.GetAccounts();
+        }
+    }
+}
---
+++ MicroServiceWithRabbitMQ/Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using Banking.Application.Interfaces;
+using Banking.Application.Services;
+using Banking.Data.Context;
+using Banking.Data.Repository;
+using Banking.Domain.Interfaces;
+using Domain.Core.Bus;
+using Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
+        }
+    }
+}
